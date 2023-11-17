3cfcaf9ecb8f0316c45cf9184de68c692eb4cfa3
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2023-11-17 11:59:07 -0300

    Corrigido function route para excecução de demais funcionalidades

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { Router } from "./router.js";
 const router = new Router();
 router.add('/', '/pages/home.html');
 router.add('/universo', '/pages/universe.html');
-router.add('/explorer', '/pages/explorer.html');
+router.add('/exploracao', '/pages/explorer.html');
 router.add(404, '/pages/404.html');
 
 router.handle()
 
 window.onpopstate = () => router.handle()
-window.route = () => router.handle()
+window.route = () => router.route();
